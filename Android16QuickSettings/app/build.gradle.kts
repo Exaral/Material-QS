2454c76f29cedf916ab790def9a68aada0c8c50c
Exaral <github.Exaral@outlook.com>
2026-08-27 21:08:32 -0300

    Delete Android16QuickSettings directory

--- Android16QuickSettings/app/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins { id("com.android.application") }
-
-android { namespace = "com.example.customqs"; compileSdk = 36
-    defaultConfig { applicationId = "com.example.customqs"; minSdk = 26; targetSdk = 36; versionCode = 1; versionName = "1.0" }
-}
